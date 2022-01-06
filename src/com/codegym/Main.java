e6b3a1a923ef27c1b0b16d053bb8cdf564388e02
CGAnVil <hoangminh.tlu@gmail.com>
2022-01-06 15:01:08 +0700

    upload bai tap

--- src/com/codegym/Main.java
+++ src/com/codegym/Main.java
@@ -1,20 +1,33 @@
 package com.codegym;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = {5,2,6,5,2,3,5,8,23};
-        int index = minValue(arr);
-        System.out.println("The smallest element in the array is: " + arr[index]);
+//        int[] arr = {5,2,6,5,2,3,5,8,23};
+//        int index = minValue(arr);
+//        System.out.println("The smallest element in the array is: " + arr[index]);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array:");
+        int size = sc.nextInt();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.println("Enter element at position : " + i);
+            array[i] = sc.nextInt();
+        }
+        int min = minValue(array);
+        System.out.println(min);
     }
 
     public static int minValue(int[] array) {
-        int index = 0;
+        int min = array[0];
         for (int i = 1; i < array.length; i++) {
-            if (array[i] < array[index]) {
-                index = i;
+            if (array[i] < min) {
+                min = array[i];
             }
         }
-        return index;
+        return min;
     }
 }
